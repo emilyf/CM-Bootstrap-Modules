87b74a3ab39c1fdb948116b66a5346842251ad2c
Kelsey Sheely <kelsey@animasweb.com>
2015-10-02 10:32:48 -0700

    vcam as a branch

---
+++ site_cp/templates/site-cp-landing-pg.tpl.php
@@ -0,0 +1,10 @@
+<ul class="control-panel">
+  <li>
+    <label>Adjust the colors on this site</label>
+    <a href="/cp/colors">Colors</a>
+  </li>
+  <li>
+    <label>Adjust Show buttons</label>
+    <a href="/cp/show-buttons">Show Buttons</a>
+  </li>
+</ul>
